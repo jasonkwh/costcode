24bf88606a252821c366f4296ba2c3f23249f150
Jason Huang <jason.huang@contactspace.com>
2018-05-02 22:52:08 +1000

    Spreadsheet Validation, Set Bold Total

--- costcodebackend.php
+++ costcodebackend.php
@@ -4,6 +4,10 @@ require 'vendor/autoload.php';
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
+function ContainsNumbers($String){
+    return preg_match('/\\d/', $String) > 0;
+}
+
 $target_dir = "xlsxdownloads/";
 if (!is_dir($target_dir)) {
     mkdir($target_dir, 0755, true);
@@ -33,6 +37,7 @@ if ($uploadOk == 0) {
         $temparray = array();
         $startworking = 0;
         $employee = "";
+        $overtimetotal = 0.00;
         for($i=1;$i<=$worksheet->getHighestRow();$i++) {
             if($startworking == 1) {
                 if((trim($worksheet->getCell('K' . $i)->getValue())!="COST CODE") && (trim($worksheet->getCell('K' . $i)->getValue())!="") && (trim($worksheet->getCell('J' . $i)->getValue())!="")) {
@@ -45,6 +50,9 @@ if ($uploadOk == 0) {
             if(trim($worksheet->getCell('A' . $i)->getValue())=="EMPLOYEE NAME:") {
                 $employee = str_replace(" ","_",str_replace("'","_",trim($worksheet->getCell('B' . $i)->getValue())));
             }
+            if(strtolower(trim($worksheet->getCell('P' . $i)->getValue()))=="overtime this pay period") {
+                $overtimetotal = (float)(trim($worksheet->getCell('S' . $i)->getCalculatedValue()));
+            }
         }
         $spreadsheet2 = new Spreadsheet();
         $sheet = $spreadsheet2->getActiveSheet();
@@ -54,12 +62,30 @@ if ($uploadOk == 0) {
         $tempindex = 0;
         $lastkey = sizeof($temparray)+1;
         $alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        $styleArray = [
+            'font' => [
+                'bold' => true,
+            ],
+            'alignment' => [
+                'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT,
+            ],
+            'borders' => [
+                'top' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                ],
+            ],
+        ];
+        $validatearray = array();
         foreach ($temparray as $key => $values) {
             $sheet->setCellValue('A' . $i, $key);
             foreach($values as $valuekey => $value) {
                 if(!in_array($valuekey,$temperarray)) {
+                    if(ContainsNumbers($valuekey)) {
+                        array_push($validatearray,$j);
+                    }
                     $sheet->setCellValueByColumnAndRow($j,1, $valuekey);
                     $sheet->setCellValueByColumnAndRow($j,$lastkey+1, "=SUM(" . (string)$alphabet[$j-1] . "2:" . (string)$alphabet[$j-1] . (string)$lastkey . ")");
+                    $sheet->getStyleByColumnAndRow($j,$lastkey+1)->applyFromArray($styleArray);
                     $sheet->getColumnDimensionByColumn($j)->setAutoSize(true);
                     array_push($temperarray,$valuekey);
                     $j++;
@@ -68,6 +94,15 @@ if ($uploadOk == 0) {
                 $sheet->setCellValueByColumnAndRow($tempindex,$i, $value);
             }
             $i++;
+        }
+        $totalhours = 0.00;
+        foreach ($validatearray as $col) {
+            $totalhours += (float)($sheet->getCellByColumnAndRow($col,$lastkey+1)->getCalculatedValue());
+        }
+        if($totalhours!=$overtimetotal) {
+            $sheet->setCellValueByColumnAndRow(1,$lastkey+3,"WARNING: PLEASE CHECK THE ORIGINAL SPREADSHEET");
+            $sheet->getStyleByColumnAndRow(1,$lastkey+3)->getFont()->setBold(true);
+            $sheet->getStyleByColumnAndRow(1,$lastkey+3)->getFont()->setColor( new \PhpOffice\PhpSpreadsheet\Style\Color( \PhpOffice\PhpSpreadsheet\Style\Color::COLOR_RED ) );
         }
         $sheet->getColumnDimensionByColumn(1)->setAutoSize(true);
         $writer = new Xlsx($spreadsheet2);
